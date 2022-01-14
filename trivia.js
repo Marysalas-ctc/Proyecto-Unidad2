e5b16fe4db4f5cae323f9bf2c7c0bca0bedb5891
Marysalas-ctc <94081671+Marysalas-ctc@users.noreply.github.com>
2022-01-13 21:52:42 -0500

    Add files via upload

---
+++ trivia.js
@@ -0,0 +1,31 @@
+
+//** caracteres especiales 
+var frase1 = `Hola:
+              quieres jugar conmigo?`;  
+
+alert(frase1);
+// el \n genera un salto de linea
+var frase2 = `Cuidado: te estamos observando`;
+alert(frase2);
+
+//Utilizando string.lenght
+
+var letras = "666666";
+var conteo = letras.length;
+alert(conteo);
+
+//Utilizando string.lenght
+
+var letras = "666666";
+var conteo = letras.length;
+alert(conteo);
+
+// accediendo al ultimo caracter 
+var  ultimo = letras[letras.length -1];
+console.log(ultimo);
+alert(ultimo);
+
+// convertir minusculas a mayusculas 
+var minus = "Estas, Ready?";
+alert(minus.toUpperCase()); 
+
